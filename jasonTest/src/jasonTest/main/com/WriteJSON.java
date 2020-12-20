87775346bda60cb8c5178d5bdbfb3e5ae7c46f4b
longjianwei@gmail.com <Administrator@PC-20180606SQDX>
2020-12-20 20:02:13 +0800

    test

---
+++ jasonTest/src/jasonTest/main/com/WriteJSON.java
@@ -0,0 +1,59 @@
+package jasonTest.main.com;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class WriteJSON {
+
+	public static void main(String[] args) {
+		 //First User
+        JSONObject userDetails = new JSONObject();
+        userDetails.put("id", 100);
+        userDetails.put("firstName", "Ramesh");
+        userDetails.put("lastName", "Fadatare");
+        userDetails.put("userName", "Ramesh Fadatare");
+        userDetails.put("email", "ramesh@gmail.com");
+
+
+        //Second user
+        JSONObject userDetails1 = new JSONObject();
+        userDetails1.put("id", 101);
+        userDetails1.put("firstName", "John");
+        userDetails1.put("lastName", "Cena");
+        userDetails1.put("userName", "John Cena");
+        userDetails1.put("email", "john@gmail.com");
+
+        // Third User
+        JSONObject userDetails2 = new JSONObject();
+        userDetails2.put("id", 102);
+        userDetails2.put("firstName", "Tony");
+        userDetails2.put("lastName", "stark");
+        userDetails2.put("userName", "Tony stark");
+        userDetails2.put("email", "tony@gmail.com");
+
+        //Add employees to list
+        JSONArray userList = new JSONArray();
+        userList.add(userDetails);
+        userList.add(userDetails1);
+        userList.add(userDetails2);
+
+        //Write JSON file
+        try (FileWriter file = new FileWriter("users.json")) {
+
+            file.write(userList.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+	}
+
+}
